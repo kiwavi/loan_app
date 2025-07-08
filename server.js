7ed821063b2d834386d4b86cc010468653195073
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-07-08 10:57:58 +0300

    chore main: base configs

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
 
+dotenv.config();
+const port = process.env.PORT;
+
+app.get("/", async (req, res) => {
+  return res.status(200).json({ success: true, message: "True" });
+});
+
+app.listen(port, () => {
+  console.log(
+    `[server]: Server is running at http://localhost:${port || 3000}`
+  );
+});
