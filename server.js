60873ec89a5323d9b17a0189c6f97f44e2d31368
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-07-08 11:51:41 +0300

    feat main: create endpoint for client creation

--- server.js
+++ server.js
@@ -19,10 +19,62 @@ app.get("/", async (req, res) => {
   return res.status(200).json({ success: true, message: "Hello world" });
 });
 
-app.post("/client", async (req, res) => {
-  try {
-  } catch (e) {}
-});
+app.post(
+  "/client",
+  body(["phone_number"])
+    .notEmpty()
+    .withMessage("Phone number is required")
+    .isMobilePhone("en-KE"),
+  body(["full_name"])
+    .notEmpty()
+    .isString()
+    .withMessage("Full name must be provided"),
+  async (req, res) => {
+    try {
+      const result = validationResult(req);
+      if (result.isEmpty()) {
+        let data = matchedData(req);
+        let { phone_number, full_name } = data;
+
+        let checkclients = await prisma.clients.findFirst({
+          where: {
+            deleted_at: null,
+            phone_number,
+          },
+        });
+
+        if (checkclients) {
+          return res
+            .status(400)
+            .json({ success: false, message: "Active client already exists" });
+        }
+
+        let client = await prisma.clients.create({
+          data: {
+            phone_number,
+            full_name,
+          },
+          select: {
+            uid: true,
+            phone_number: true,
+            full_name: true,
+          },
+        });
+
+        return res.status(200).json({ success: true, data: client });
+      }
+
+      return res.status(400).json({
+        message: "validation failed",
+        validationErrors: result
+          .array()
+          .map((error) => `${error.path}(${error.location}): ${error.msg}`),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
 
 app.listen(port, () => {
   console.log(
