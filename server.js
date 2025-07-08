42e54341e4a3b72ea5e0d845a7e06046e4adf87b
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-07-08 11:34:29 +0300

    feat main: install prisma client

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const {
+  query,
+  body,
+  matchedData,
+  validationResult,
+} = require("express-validator");
+const prisma = require("./prisma/client");
 
 app.use(bodyParser.json());
 
@@ -10,6 +17,11 @@ const port = process.env.PORT;
 
 app.get("/", async (req, res) => {
   return res.status(200).json({ success: true, message: "Hello world" });
+});
+
+app.post("/client", async (req, res) => {
+  try {
+  } catch (e) {}
 });
 
 app.listen(port, () => {
